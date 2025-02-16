23ce9deadae9758f4d08041e49f5736a3074ddb9
sbharathidasan <bharathidasan2072005@gmail.com>
2025-02-16 16:52:43 +0530

    example for compile time overloading

---
+++ polymorphism/compile time polymorphism/methodoverloading.java
@@ -0,0 +1,16 @@
+class Operation{
+static double dAdd(double a,double b){
+    return a+b;
+}
+static int add(int a,int b){
+    return a+b;
+}
+}
+class methodoverloading{
+    public static void main(String[] args) {
+        System.out.println(Operation.add(12,12));
+        System.out.println(Operation.dAdd(12.40,12.55));
+        
+    }
+    
+}
