d392d564bc6d0adb77a2c1745d83ec38c61add87
sbharathidasan <bharathidasan2072005@gmail.com>
2025-03-02 22:11:53 +0530

    doubts clarification

---
+++ encapsulation/statnonstat.java
@@ -0,0 +1,11 @@
+class statnonstat{
+static String name="hi";
+public static void main (String[] args){
+    System.out.println(name);
+    encap n=new encap();
+    encap.print();
+}
+void print(){
+ System.out.println(name);
+}
+}
